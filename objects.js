78344f47eb650ebc5816c4a5da8cc3fa4d02d943
honorable-scraper-2425 <kdraper93@gmail.com>
2019-06-28 18:11:54 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ function updatePlaylist(obj, key, value) {
   return obj
 }
 
-function removeFromPlaylist(obj, key) {
-  delete playlist.key;
+function removeFromPlaylist(playlist, key, value) {
+  delete playlist.key
   return playlist
 }
