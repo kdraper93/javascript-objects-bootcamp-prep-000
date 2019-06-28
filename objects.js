8ae9a28f4d6e94e6b0a351b099f93ee697c012dc
honorable-scraper-2425 <kdraper93@gmail.com>
2019-06-28 18:06:47 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {Hanson:"MmmBop"};
 
-function updatePlaylist(obj, key, value){
-  playlist.key= 'value';
+function updatePlaylist(obj, key, value) {
+  playlist.key = 'value';
   return playlist
 }
